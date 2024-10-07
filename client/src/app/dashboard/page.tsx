8bc3ec565d5794df715ef51bf886b88cdb2ffc53
Koshik <koshikgurbani@gmail.com>
2024-10-07 22:22:42 +0530

    Added Popular Products Card Component in the dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -3,9 +3,22 @@
 // type Props = {}
 
 "use client"
+
+import CardPopularProducts from "./CardPopularProducts"
+
 const Dashboard = (/* props: Props */) => {
   return (
-    <div>Dashboard</div>
+    <div
+      className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      {/* <div className="row-span-3 xl:row-span-6 bg-gray-500" /> */}
+      <CardPopularProducts />
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500" />
+      <div className="row-span-3 bg-gray-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
+    </div>
   )
 }
 
